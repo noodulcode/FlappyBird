7d528efe0380763e56e6eaa191113af5a6180960
noodulcode <gcab012203@coderacademy.edu.au>
2022-05-25 12:31:55 +1000

    animate player sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
    private Vector3 direction;
 
    public float gravity = -9.8f;
    public float strength = 5f;
+
+   private void Awake() 
+   {
+       spriteRenderer = GetComponent<SpriteRenderer>();
+   }
+
+   private void Start() 
+   {
+       InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f); // calls the animatesprite function every 0.15 seconds
+   }
 
    private void Update() 
    {
@@ -29,4 +42,19 @@ public class Player : MonoBehaviour
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
        }
+    
+    private void AnimateSprite() // increasing the index and then reassigning the sprite on sprite renderer based on the sprite index in array
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length) // need to make sure it loops back to beginning when we get to end of array
+        {
+            spriteIndex = 0; // starts over
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex]; // update sprites
+    }
+
+
+
    }
